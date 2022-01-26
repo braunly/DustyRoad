565dbf5c3b67b9faa71d009f13e0ee34cf91e3ac
Braunly <braunly.brony@gmail.com>
2022-01-26 16:17:10 +0200

    Rename user-agent

--- launcher-bootstrap/src/main/java/com/skcraft/launcher/bootstrap/HttpRequest.java
+++ launcher-bootstrap/src/main/java/com/skcraft/launcher/bootstrap/HttpRequest.java
@@ -120,7 +120,7 @@ public class HttpRequest implements Closeable, ProgressObservable {
         }
 
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Java) SKMCLauncher");
+        conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Java) YabKoDustyRoad");
         conn.setInstanceFollowRedirects(false);
 
         if (body != null) {
