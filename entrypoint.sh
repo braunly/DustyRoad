f294d5b837e6297fd8664c698a10c5d38cb7236d
Braunly <braunly.brony@gmail.com>
2022-08-14 15:49:50 +0300

    feat(ci): add jq to modpack-builder docker image

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 manifest_name=$1
+version=$2
 
 java -jar /app/modpack-builder.jar \
-    --version "$(date +%s)" \
+    --version "${version}" \
     --input /app/modpack \
     --output /app/upload \
     --manifest-dest "/app/upload/${manifest_name}.json"
